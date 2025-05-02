59b16dcd06c804076e5d53d200c4f5e3b7830d1c
vgabrielk <vfelix1553@gmail.com>
2025-05-02 14:02:40 -0300

    feat: Creating user routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(UserController::class)->prefix('users')->name('users.')->group(function () {
+    Route::get('', [UserController::class, 'index'])->name('index');
+    Route::get('/view', [UserController::class, 'view'])->name('view');
+    Route::get('/create', [UserController::class, 'create'])->name('create');
+    Route::get('/edit/{id}', [UserController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [UserController::class, 'update'])->name('update');
+    Route::delete('/delete/{id}', [UserController::class, 'destroy'])->name('delete');
+    Route::post('', [UserController::class, 'store'])->name('store');
 });
