1a39fe398c4699279d58f5fe7702cdc0a1de6315
VITOR FELIX <76710844+vgabrielk@users.noreply.github.com>
2025-05-05 11:20:26 -0300

    feat: Improving layout UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,32 +7,85 @@
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
 
-    <script src="{{ asset('vendor/bladewind/js/bladewind-ui.min.js') }}"></script>
-
-    <script defer src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
-    <script defer src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
+    <script src="https://unpkg.com/lucide@latest"></script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 text-gray-900 font-sans min-h-screen antialiased">
 
-    <header class="bg-white shadow p-4 px-12 mb-12" >
-        <div class="mx-auto flex justify-between">
-            <h1 class="text-xl font-bold">@yield('header', 'Facilita')</h1>
-           <div class="flex items-center gap-4">
-               <a href="{{route('users.view')}}" >Users</a>
-               <a href="{{route('genres.view')}}" >Genres</a>
-               <a href="{{route('books.view')}}" >Books</a>
-           </div>
+<div x-data="{ open: false }" class="flex h-screen">
 
+    <div :class="{'translate-x-0': open, '-translate-x-full': !open}" class="lg:hidden fixed inset-0 z-50 transition-transform duration-300 bg-gray-800 bg-opacity-75">
+        <div class="flex justify-end p-4">
+            <button @click="open = false" class="text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
         </div>
-    </header>
+        <div class="flex flex-col items-center space-y-4 text-white">
+            <a href="{{ route('users.view') }}" class="hover:text-gray-300">Usuários</a>
+            <a href="{{ route('loans.index') }}" class="hover:text-gray-300">Empréstimos</a>
+            <a href="{{ route('books.view') }}" class="hover:text-gray-300">Livros</a>
+        </div>
+    </div>
 
-    <main class=" mx-auto px-12">
-        @yield('content')
-    </main>
+    <div class="flex-1 overflow-y-auto">
+
+        <header class="bg-white py-6 px-12 shadow-sm mb-12 flex justify-between items-center">
+            <div class="flex items-center gap-4">
+                <div>
+                    <h2 class="text-lg font-semibold text-gray-900">Facilita</h2>
+                </div>
+            </div>
+
+            <div class="hidden flex items-center" id="button-menu">
+                <button @click="open = !open" class="text-gray-900">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </div>
+
+            <div class="lg:flex items-center gap-6 text-blue-500" id="navigation-menu">
+                <nav class="flex flex-wrap items-center gap-4 text-sm font-medium text-gray-600">
+                    <a href="{{ route('users.view') }}" class="hover:text-gray-900 transition">Usuários</a>
+                    <a href="{{ route('loans.index') }}" class="hover:text-gray-900 transition">Empréstimos</a>
+                    <a href="{{ route('books.view') }}" class="hover:text-gray-900 transition">Livros</a>
+                </nav>
+            </div>
+        </header>
+
+        <div x-show="open" class="hidden">
+            <nav class="space-y-4 px-6 py-4 text-white">
+                <a href="{{ route('users.view') }}" class="block hover:text-gray-300">Usuários</a>
+                <a href="{{ route('loans.index') }}" class="block hover:text-gray-300">Empréstimos</a>
+                <a href="{{ route('books.view') }}" class="block hover:text-gray-300">Livros</a>
+            </nav>
+        </div>
+
+        <main class="mx-auto px-12">
+            @yield('content')
+        </main>
+    </div>
+</div>
+
+<script>
+    lucide.createIcons();
+</script>
+<style>
+@media(max-width: 768px){
+    #button-menu{
+        display: initial;
+    }
+    #navigation-menu{
+        display: none;
+    }
+}
+</style>
+
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,61 @@
 @extends('layouts.app')
 
-
-@section('title', 'Página Inicial')
-
-@section('header', 'Facilita App')
+@section('title', 'Dashboard')
+@section('header', 'Gráfico de Barras')
 
 @section('content')
+    <div class="max-w-full bg-white rounded-lg shadow-sm dark:bg-gray-800 p-4 md:p-6">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div class="bg-white rounded-lg shadow-sm dark:bg-gray-800 p-4 md:p-6">
+                <div class="flex justify-between pb-4 mb-4 border-b border-gray-200 dark:border-gray-700">
+                    <div class="flex items-center">
+                        <div class="w-12 h-12 rounded-lg bg-gray-100 dark:bg-gray-700 flex items-center justify-center me-3">
+                            <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 19">
+                                <path d="M14.5 0A3.987 3.987 0 0 0 11 2.1a4.977 4.977 0 0 1 3.9 5.858A3.989 3.989 0 0 0 14.5 0ZM9 13h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"/>
+                                <path d="M5 19h10v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2ZM5 7a5.008 5.008 0 0 1 4-4.9 3.988 3.988 0 1 0-3.9 5.859A4.974 4.974 0 0 1 5 7Zm5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm5-1h-.424a5.016 5.016 0 0 1-1.942 2.232A6.007 6.007 0 0 1 17 17h2a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5ZM5.424 9H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h2a6.007 6.007 0 0 1 4.366-5.768A5.016 5.016 0 0 1 5.424 9Z"/>
+                            </svg>
+                        </div>
+                        <div>
+                            <h5 class="leading-none text-2xl font-bold text-gray-900 dark:text-white pb-1">{{ $loanCount }}</h5>
+                            <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Empréstimos realizados</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="bg-white rounded-lg shadow-sm dark:bg-gray-800 p-4 md:p-6">
+                <div class="flex justify-between pb-4 mb-4 border-b border-gray-200 dark:border-gray-700">
+                    <div class="flex items-center">
+                        <div class="w-12 h-12 rounded-lg bg-gray-100 dark:bg-gray-700 flex items-center justify-center me-3">
+                            <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 19">
+                                <path d="M3 2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H3Zm1 2h12v12H4V4Z"/>
+                            </svg>
+                        </div>
+                        <div>
+                            <h5 class="leading-none text-2xl font-bold text-gray-900 dark:text-white pb-1">{{ $bookCount }}</h5>
+                            <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Livros registrados</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bg-white rounded-lg shadow-sm dark:bg-gray-800 p-4 md:p-6">
+                <div class="flex justify-between pb-4 mb-4 border-b border-gray-200 dark:border-gray-700">
+                    <div class="flex items-center">
+                        <div class="w-12 h-12 rounded-lg bg-gray-100 dark:bg-gray-700 flex items-center justify-center me-3">
+                            <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 19">
+                                <path d="M14 3a3 3 0 1 0-6 0 3 3 0 0 0 6 0ZM10 5a1 1 0 1 1 0 2 1 1 0 0 1 0-2Zm0 4a4 4 0 0 0-4 4v4h8v-4a4 4 0 0 0-4-4Zm2 7H8v-3a2 2 0 0 1 4 0v3Z"/>
+                            </svg>
+                        </div>
+                        <div>
+                            <h5 class="leading-none text-2xl font-bold text-gray-900 dark:text-white pb-1">{{ $userCount }}</h5>
+                            <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Usuários registrados</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="column-chart"></div>
+    </div>
 @endsection
